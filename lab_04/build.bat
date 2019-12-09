7a38bb0f294ad247beabfdf20b404f507513407a
SGCube <sgcube@yandex.ru>
2019-12-09 16:51:58 +0300

    lab_04: all but user type

---
+++ lab_04/build.bat
@@ -0,0 +1,1 @@
+%SYSTEMROOT%\Microsoft.NET\Framework\v2.0.50727\csc.exe /target:library SqlUserFunctions.cs
---
+++ lab_04/sql_clr/SqlTrigger1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using Microsoft.SqlServer.Server;
+
+public partial class Triggers
+{
+    // [Microsoft.SqlServer.Server.SqlTrigger(Name="NoLineDelete", Target="[Lines]", Event="FOR UPDATE")]
+    public static void T_NoLineDelete()
+    {
+        SqlTriggerContext triggerContext = SqlContext.TriggerContext;
+        if (triggerContext.TriggerAction == TriggerAction.Delete)
+            SqlContext.Pipe.Send("You can't delete lines.");
+    }
+}
+
